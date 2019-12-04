d5e912605172b2f725ec22232089be7b850867a9
André Bauer <monotek23@gmail.com>
2019-12-04 22:36:23 +0100

    test

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -9,15 +9,12 @@ CONFIG_DIR=".ci"
 GIT_REPO="https://github.com/monotek/helm-charts"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-echo "$REPO_ROOT"
+ls -al
+pwd
 
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
 
-git remote -v
-git log
-
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
   --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml
-
