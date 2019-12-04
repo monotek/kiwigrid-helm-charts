83a77cf19a300f49e29d9d50e6c85502196c76dd
André Bauer <monotek23@gmail.com>
2019-12-04 16:56:56 +0100

    test

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -13,6 +13,10 @@ echo $REPO_ROOT
 
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
+
+git remote -v
+git log
+
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
   --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml \
