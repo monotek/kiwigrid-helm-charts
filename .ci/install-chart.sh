acf56acb20d2c83df0e46f4a4eee7f125e4b5502
André Bauer <monotek23@gmail.com>
2019-12-04 16:47:22 +0100

    test

--- .ci/install-chart.sh
+++ .ci/install-chart.sh
@@ -11,125 +11,121 @@ WORKDIR="/workdir"
 CLUSTER_NAME="chart-testing"
 DOCKER_NAME="ct"
 
-if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
-  echo -e "\\nTesting in Kubernetes ${K8S_VERSION}\\n"
 
-  run_ct_container() {
-      echo "Running ${DOCKER_NAME} container..."
+echo -e "\\nTesting in Kubernetes ${K8S_VERSION}\\n"
 
-      docker container run --rm --interactive --detach --network host --name "${DOCKER_NAME}" \
-          --volume "${REPO_ROOT}/.circleci/ct.yaml:/etc/ct/ct.yaml" \
-          --volume "${REPO_ROOT}:${WORKDIR}" \
-          --workdir ${WORKDIR} \
-          "${CHART_TESTING_IMAGE}:${CHART_TESTING_TAG}" \
-          cat
-      echo
-  }
+run_ct_container() {
+    echo "Running ${DOCKER_NAME} container..."
 
-  cleanup() {
-      echo "Removing ${DOCKER_NAME} container..."
+    docker container run --rm --interactive --detach --network host --name "${DOCKER_NAME}" \
+        --volume "${REPO_ROOT}/.circleci/ct.yaml:/etc/ct/ct.yaml" \
+        --volume "${REPO_ROOT}:${WORKDIR}" \
+        --workdir ${WORKDIR} \
+        "${CHART_TESTING_IMAGE}:${CHART_TESTING_TAG}" \
+        cat
+    echo
+}
 
-      docker container kill "${DOCKER_NAME}" > /dev/null 2>&1
+cleanup() {
+    echo "Removing ${DOCKER_NAME} container..."
 
-      echo 'Done!'
-  }
+    docker container kill "${DOCKER_NAME}" > /dev/null 2>&1
 
-  docker_exec() {
-      docker container exec --interactive ct "$@"
-  }
+    echo 'Done!'
+}
 
-  create_kind_cluster() {
-      echo 'Installing kind...'
+docker_exec() {
+    docker container exec --interactive ct "$@"
+}
 
-      curl -sSLo kind "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-linux-amd64"
-      chmod +x kind
-      sudo mv kind /usr/local/bin/kind
+create_kind_cluster() {
+    echo 'Installing kind...'
 
-      kind create cluster --name "${CLUSTER_NAME}" --config "${REPO_ROOT}"/.circleci/kind-config.yaml --image "kindest/node:${K8S_VERSION}"
+    curl -sSLo kind "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-linux-amd64"
+    chmod +x kind
+    sudo mv kind /usr/local/bin/kind
 
-      docker_exec mkdir -p /root/.kube
+    kind create cluster --name "${CLUSTER_NAME}" --config "${REPO_ROOT}"/.circleci/kind-config.yaml --image "kindest/node:${K8S_VERSION}"
 
-      echo 'Copying kubeconfig to container...'
-      local KUBECONFIG
-      KUBECONFIG="$(kind get kubeconfig-path --name "${CLUSTER_NAME}")"
-      docker cp "${KUBECONFIG}" "${DOCKER_NAME}":/root/.kube/config
+    docker_exec mkdir -p /root/.kube
 
-      docker_exec kubectl cluster-info
-      echo
+    echo 'Copying kubeconfig to container...'
+    local KUBECONFIG
+    KUBECONFIG="$(kind get kubeconfig-path --name "${CLUSTER_NAME}")"
+    docker cp "${KUBECONFIG}" "${DOCKER_NAME}":/root/.kube/config
 
-      echo -n 'Waiting for cluster to be ready...'
-      until ! grep --quiet 'NotReady' <(docker_exec kubectl get nodes --no-headers); do
-          printf '.'
-          sleep 1
-      done
+    docker_exec kubectl cluster-info
+    echo
 
-      echo '✔︎'
-      echo
+    echo -n 'Waiting for cluster to be ready...'
+    until ! grep --quiet 'NotReady' <(docker_exec kubectl get nodes --no-headers); do
+        printf '.'
+        sleep 1
+    done
 
-      docker_exec kubectl get nodes
-      echo
+    echo '✔︎'
+    echo
 
-      echo 'Cluster ready!'
-      echo
-  }
+    docker_exec kubectl get nodes
+    echo
 
-  install_tiller() {
-      echo 'Installing Tiller...'
-      docker_exec kubectl --namespace kube-system create sa tiller
-      docker_exec kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-      docker_exec helm init --service-account tiller --upgrade --wait
-      echo
-  }
+    echo 'Cluster ready!'
+    echo
+}
 
-  install_hostpath-provisioner() {
-      # kind doesn't support Dynamic PVC provisioning yet, this is one way to get it working
-      # https://github.com/rimusz/charts/tree/master/stable/hostpath-provisioner
+install_tiller() {
+    echo 'Installing Tiller...'
+    docker_exec kubectl --namespace kube-system create sa tiller
+    docker_exec kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    docker_exec helm init --service-account tiller --upgrade --wait
+    echo
+}
 
-      echo 'Installing hostpath-provisioner...'
+install_hostpath-provisioner() {
+    # kind doesn't support Dynamic PVC provisioning yet, this is one way to get it working
+    # https://github.com/rimusz/charts/tree/master/stable/hostpath-provisioner
 
-      # Remove default storage class. Will be recreated by hostpath -provisioner
-      docker_exec kubectl delete storageclass standard
+    echo 'Installing hostpath-provisioner...'
 
-      docker_exec helm repo add rimusz https://charts.rimusz.net
-      docker_exec helm repo update
-      docker_exec helm install rimusz/hostpath-provisioner --name hostpath-provisioner --namespace kube-system --wait
-      echo
-  }
+    # Remove default storage class. Will be recreated by hostpath -provisioner
+    docker_exec kubectl delete storageclass standard
 
-  add_helm_repos() {
-      docker_exec helm repo add kiwigrid https://kiwigrid.github.io/
-      docker_exec helm repo update
-      echo
-  }
+    docker_exec helm repo add rimusz https://charts.rimusz.net
+    docker_exec helm repo update
+    docker_exec helm install rimusz/hostpath-provisioner --name hostpath-provisioner --namespace kube-system --wait
+    echo
+}
 
-  install_charts() {
-      docker_exec "${DOCKER_NAME}" install --config=${WORKDIR}/.circleci/ct.yaml
-      echo
-  }
+add_helm_repos() {
+    docker_exec helm repo add kiwigrid https://kiwigrid.github.io/
+    docker_exec helm repo update
+    echo
+}
 
-  cleanup_cluster() {
-    if [ -n "$(command -v kind)" ]; then
-      for CLUSTER in $(kind get clusters); do
-        echo "delete old cluster ${CLUSTER}"
-        kind delete cluster --name "${CLUSTER}"
-      done
-    fi
-  }
+install_charts() {
+    docker_exec "${DOCKER_NAME}" install --config=${WORKDIR}/.circleci/ct.yaml
+    echo
+}
 
-  main() {
-      cleanup_cluster
-      create_kind_cluster
-      install_tiller
-      install_hostpath-provisioner
-      add_helm_repos
-      install_charts
-  }
-
-  run_ct_container
-  trap cleanup EXIT
-
-  main
-
-else
-  echo "skipped chart install as its not a pull request..."
+cleanup_cluster() {
+if [ -n "$(command -v kind)" ]; then
+    for CLUSTER in $(kind get clusters); do
+    echo "delete old cluster ${CLUSTER}"
+    kind delete cluster --name "${CLUSTER}"
+    done
 fi
+}
+
+main() {
+    cleanup_cluster
+    create_kind_cluster
+    install_tiller
+    install_hostpath-provisioner
+    add_helm_repos
+    install_charts
+}
+
+run_ct_container
+trap cleanup EXIT
+
+main
