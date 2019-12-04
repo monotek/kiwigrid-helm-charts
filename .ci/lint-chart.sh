c26ccbd94db723ea022f60e4aba6205b1bc270b4
André Bauer <monotek23@gmail.com>
2019-12-04 22:10:13 +0100

    test

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -19,5 +19,5 @@ git log
 
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
-  --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml \
-  --debug
+  --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml
+
